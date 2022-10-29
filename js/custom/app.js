37b431c11e8a85a0cfe5a504c42aae7182f957cb
Pham Van Tuan <phamvantuan2304@gmail.com>
2022-10-29 17:30:27 +0700

    update

--- js/custom/app.js
+++ js/custom/app.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
   const getMenuChild = (items) => {
-    return `<ul>
-            ${items.map((item) => `<li><a href="#">${item.name}</a></li>`)}
-        </ul>`;
+    let html = "";
+    items.map((item) => {
+      html += `<li><a href="#">${item.name}</a></li>`;
+    });
+    return `<ul>${html}</ul>`;
   };
-
   const MenuItem = (item) => {
     return `<li>
               <a href="${item.link}">${item.name}</a>
@@ -12,29 +13,28 @@ $(document).ready(function () {
   };
   const CategoryItem = (item) => {
     return `<li class="closed"><a href="#">${item.name}</a>
-              <ul>
-                ${getMenuChild(item.children)}
-              </ul>
+              ${getMenuChild(item.children)}
             </li>`;
   };
   const ProductItem = (item) => {
     return `<div class="col-md-4 col-sm-4 col-xs-12">
               <div class="product-item">
                 <div class="product-img">
-                  <a href="single-product.html">
+                  <a href="">
                     <img src="${item.image}" alt="" />
                   </a>
                 </div>
                 <div class="product-info">
                   <h6 class="product-title">
-                    <a href="single-product.html">${item.name} </a>
+                    <a href="">${item.name} </a>
                   </h6>
                   <div class="pro-rating">
                     <a href="#"><i class="zmdi zmdi-star"></i></a>
                     <a href="#"><i class="zmdi zmdi-star"></i></a>
                     <a href="#"><i class="zmdi zmdi-star"></i></a>
+                    <a href="#"><i class="zmdi zmdi-star"></i></a>
+                    <a href="#"><i class="zmdi zmdi-star"></i></a>
                     <a href="#"><i class="zmdi zmdi-star-half"></i></a>
-                    <a href="#"><i class="zmdi zmdi-star-outline"></i></a>
                   </div>
                   <h3 class="pro-price">${item.price}</h3>
                   <ul class="action-button">
@@ -42,7 +42,7 @@ $(document).ready(function () {
                       <a href="#" title="Wishlist"><i class="zmdi zmdi-favorite"></i></a>
                     </li>
                     <li>
-                      <a href="#" data-toggle="modal" data-target="#productModal" title="Xem thêm"><i
+                      <a href="#" title="Xem thêm"><i
                           class="zmdi zmdi-zoom-in"></i></a>
                     </li>
                     <li>
@@ -58,167 +58,68 @@ $(document).ready(function () {
   };
   const ProductItem2 = (item) => {
     return `<div class="col-md-12">
-                        <div class="shop-list product-item">
-                          <div class="product-img">
-                            <a href="single-product.html">
-                              <img src="${item.image}" alt="" />
-                            </a>
-                          </div>
-                          <div class="product-info">
-                            <div class="clearfix">
-                              <h6 class="product-title f-left">
-                                <a href="single-product.html">${item.name} </a>
-                              </h6>
-                              <div class="pro-rating f-right">
-                                <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                <a href="#"><i class="zmdi zmdi-star-half"></i></a>
-                                <a href="#"><i class="zmdi zmdi-star-outline"></i></a>
-                              </div>
-                            </div>
-                            <h6 class="brand-name mb-30">Brand Name</h6>
-                            <h3 class="pro-price">${item.price}</h3>
-                            <p>${item.description}</p>
-                            <ul class="action-button">
-                              <li>
-                                <a href="#" title="Wishlist"><i class="zmdi zmdi-favorite"></i></a>
-                              </li>
-                              <li>
-                                <a href="#" data-toggle="modal" data-target="#productModal" title="Xem thêm"><i
-                                    class="zmdi zmdi-zoom-in"></i></a>
-                              </li>
-                              <li>
-                                <a href="#" title="Compare"><i class="zmdi zmdi-refresh"></i></a>
-                              </li>
-                              <li>
-                                <a href="#" title="Add to cart"><i class="zmdi zmdi-shopping-cart-plus"></i></a>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div>`;
+              <div class="shop-list product-item">
+                <div class="product-img">
+                  <a href="">
+                    <img src="${item.image}" alt="" />
+                  </a>
+                </div>
+                <div class="product-info">
+                  <div class="clearfix">
+                    <h6 class="product-title f-left">
+                      <a href="">${item.name} </a>
+                    </h6>
+                    <div class="pro-rating f-right">
+                      <a href="#"><i class="zmdi zmdi-star"></i></a>
+                      <a href="#"><i class="zmdi zmdi-star"></i></a>
+                      <a href="#"><i class="zmdi zmdi-star"></i></a>
+                      <a href="#"><i class="zmdi zmdi-star"></i></a>
+                      <a href="#"><i class="zmdi zmdi-star-half"></i></a>
+                    </div>
+                  </div>
+                  <h6 class="brand-name mb-30">Brand Name</h6>
+                  <h3 class="pro-price">${item.price}</h3>
+                  <p>${item.description}</p>
+                  <ul class="action-button">
+                    <li>
+                      <a href="#" title="Wishlist"><i class="zmdi zmdi-favorite"></i></a>
+                    </li>
+                    <li>
+                      <a href="#" title="Xem thêm"><i
+                          class="zmdi zmdi-zoom-in"></i></a>
+                    </li>
+                    <li>
+                      <a href="#" title="Compare"><i class="zmdi zmdi-refresh"></i></a>
+                    </li>
+                    <li>
+                      <a href="#" title="Add to cart"><i class="zmdi zmdi-shopping-cart-plus"></i></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>`;
   };
   const ProductItem3 = (item) => {
     return `<div class="product-item">
                   <div class="product-img">
-                    <a href="single-product.html">
+                    <a href="">
                       <img src="${item.image}" alt="" />
                     </a>
                   </div>
                   <div class="product-info">
                     <h6 class="product-title">
-                      <a href="single-product.html">${item.name}</a>
+                      <a href="">${item.name}</a>
                     </h6>
                     <h3 class="pro-price">${item.price}</h3>
                   </div>
                 </div>`;
   };
-  const CardItem = () => {
-    return `<div class="total-cart-in">
-                      <div class="cart-toggler">
-                        <a href="#">
-                          <span class="cart-quantity">02</span><br>
-                          <span class="cart-icon">
-                            <i class="zmdi zmdi-shopping-cart-plus"></i>
-                          </span>
-                        </a>
-                      </div>
-                      <ul>
-                        <li>
-                          <div class="top-cart-inner your-cart">
-                            <h5 class="text-capitalize">Your Cart</h5>
-                          </div>
-                        </li>
-                        <li>
-                          <div class="total-cart-pro">
-                            <!-- single-cart -->
-                            <div class="single-cart clearfix">
-                              <div class="cart-img f-left">
-                                <a href="#">
-                                  <img src="img/cart/1.jpg" alt="Cart Product" />
-                                </a>
-                                <div class="del-icon">
-                                  <a href="#">
-                                    <i class="zmdi zmdi-close"></i>
-                                  </a>
-                                </div>
-                              </div>
-                              <div class="cart-info f-left">
-                                <h6 class="text-capitalize">
-                                  <a href="#">Dummy Product Name</a>
-                                </h6>
-                                <p>
-                                  <span>Brand <strong>:</strong></span>Brand Name
-                                </p>
-                                <p>
-                                  <span>Model <strong>:</strong></span>Grand s2
-                                </p>
-                                <p>
-                                  <span>Color <strong>:</strong></span>Black, White
-                                </p>
-                              </div>
-                            </div>
-                            <!-- single-cart -->
-                            <div class="single-cart clearfix">
-                              <div class="cart-img f-left">
-                                <a href="#">
-                                  <img src="img/cart/1.jpg" alt="Cart Product" />
-                                </a>
-                                <div class="del-icon">
-                                  <a href="#">
-                                    <i class="zmdi zmdi-close"></i>
-                                  </a>
-                                </div>
-                              </div>
-                              <div class="cart-info f-left">
-                                <h6 class="text-capitalize">
-                                  <a href="#">Dummy Product Name</a>
-                                </h6>
-                                <p>
-                                  <span>Brand <strong>:</strong></span>Brand Name
-                                </p>
-                                <p>
-                                  <span>Model <strong>:</strong></span>Grand s2
-                                </p>
-                                <p>
-                                  <span>Color <strong>:</strong></span>Black, White
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </li>
-                        <li>
-                          <div class="top-cart-inner subtotal">
-                            <h4 class="text-uppercase g-font-2">
-                              Total =
-                              <span>$ 500.00</span>
-                            </h4>
-                          </div>
-                        </li>
-                        <li>
-                          <div class="top-cart-inner view-cart">
-                            <h4 class="text-uppercase">
-                              <a href="#">View cart</a>
-                            </h4>
-                          </div>
-                        </li>
-                        <li>
-                          <div class="top-cart-inner check-out">
-                            <h4 class="text-uppercase">
-                              <a href="#">Check out</a>
-                            </h4>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>`;
-  };
 
   const menus = [
-    { id: 1, name: "Giới thiệu", link: "about.html", numberOder: 1 },
-    { id: 2, name: "Bảng giá", link: "price.html", numberOder: 2 },
+    { id: 1, name: "Giới thiệu", link: "", numberOder: 1 },
+    { id: 2, name: "Bảng giá", link: "", numberOder: 2 },
     { id: 3, name: "Sản phẩm", link: "", numberOder: 3 },
-    { id: 4, name: "Liên hệ", link: "contact.html", numberOder: 4 },
+    { id: 4, name: "Liên hệ", link: "", numberOder: 4 },
   ];
   const categories = [
     {
@@ -239,6 +140,12 @@ $(document).ready(function () {
           description: "",
           image: "",
         },
+        {
+          id: 12,
+          name: "Cá gia vị",
+          description: "",
+          image: "",
+        },
       ],
     },
     {
@@ -246,7 +153,20 @@ $(document).ready(function () {
       name: "Mực",
       description: "",
       image: "",
-      children: [],
+      children: [
+        {
+          id: 20,
+          name: "Mực Khô",
+          description: "",
+          image: "",
+        },
+        {
+          id: 21,
+          name: "Mực 1 nắng",
+          description: "",
+          image: "",
+        },
+      ],
     },
   ];
 
@@ -255,6 +175,8 @@ $(document).ready(function () {
       id: 1,
       name: "Cá lóc khô",
       description: "",
+      numberOder: 1,
+      isHot: true,
       price: 10_000,
       priceText: "10_000",
       oldPrice: 15_000,
@@ -271,12 +193,14 @@ $(document).ready(function () {
       id: 2,
       name: "Cá chỉ vàng",
       description: "",
+      numberOder: 2,
+      isHot: true,
       price: 10_000,
       priceText: "10_000",
       oldPrice: 15_000,
       oldPrice: "15_000",
       rate: 4,
-      image: "https://haisankhanhhoa.vn/cms-content/uploads/thumbs/ca-com-dac-biet_hskh-md.jpg",
+      image: "https://haisankhanhhoa.vn/cms-content/uploads/thumbs/ca-kho/kho-ca-chi-vang-hskh-md.jpg",
       isActive: true,
       isSale: true,
       guide: "",
@@ -287,6 +211,8 @@ $(document).ready(function () {
       id: 3,
       name: "Cá khoai khô",
       description: "",
+      numberOder: 3,
+      isHot: true,
       price: 10_000,
       priceText: "10_000",
       oldPrice: 15_000,
@@ -303,6 +229,8 @@ $(document).ready(function () {
       id: 4,
       name: "Cá khoai khô",
       description: "",
+      numberOder: 4,
+      isHot: false,
       price: 10_000,
       priceText: "10_000",
       oldPrice: 15_000,
@@ -319,12 +247,14 @@ $(document).ready(function () {
       id: 5,
       name: "Cá khoai khô",
       description: "",
+      numberOder: 5,
+      isHot: false,
       price: 10_000,
       priceText: "10_000",
       oldPrice: 15_000,
       oldPrice: "15_000",
       rate: 4,
-      image: "https://haisankhanhhoa.vn/cms-content/uploads/thumbs/ca-kho/kho-ca-duoi_hskh-md.jpg",
+      image: "https://haisankhanhhoa.vn/cms-content/uploads/thumbs/ca-com-dac-biet_hskh-md.jpg",
       isActive: true,
       isSale: true,
       guide: "",
@@ -333,8 +263,10 @@ $(document).ready(function () {
     },
     {
       id: 6,
-      name: "Cá khoai khô",
+      name: "Cá khoai khô 6",
       description: "",
+      numberOder: 6,
+      isHot: false,
       price: 10_000,
       priceText: "10_000",
       oldPrice: 15_000,
@@ -356,11 +288,20 @@ $(document).ready(function () {
       App.renderHomeProducts();
       App.renderHotProducts();
     },
-    getProducts: () => {
-      return products;
+    getProducts: (filter = {}) => {
+      let tmpProducts = [...products, ...products, ...products, ...products, ...products, ...products];
+
+      if (filter?.isNumberOrder) {
+        tmpProducts = tmpProducts.sort((a, b) => a.orderNumber - b.orderNumber);
+      }
+
+      if (filter?.isHot) {
+        tmpProducts = tmpProducts.filter((item) => item.isHot).sort((a, b) => (a.orderNumber > b.orderNumber ? 1 : -1));
+      }
+      return tmpProducts;
     },
     renderHomeProducts: () => {
-      const products = App.getProducts();
+      const products = App.getProducts({ isNumberOrder: true });
       App.renderProducts(products);
     },
     renderProducts: (products) => {
@@ -375,6 +316,7 @@ $(document).ready(function () {
     },
     renderHotProducts: () => {
       let htmlProducts = "";
+      const products = App.getProducts({ isHot: true });
       products.map((item) => {
         htmlProducts += ProductItem3(item);
       });
